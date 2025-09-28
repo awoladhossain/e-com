178dcaf348ccb0baa17a266f96770aa6f7c8576b
Awolad Hossain <awoladh04@gmail.com>
2025-09-29 00:36:06 +0600

    bug fixed

--- server/src/reviews/review.routes.js
+++ server/src/reviews/review.routes.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { verifyToken } from "../middleware/verifyToken.js";
 import {
   createPost,
   getTotalReviews,
@@ -7,8 +8,8 @@ import {
 
 const router = express.Router();
 
-router.post("/post-review", createPost);
-router.get("/get-reviews-count", getTotalReviews);
-router.get("/:userId", getUserReviews);
+router.post("/post-review", verifyToken, createPost);
+router.get("/get-reviews-count", verifyToken, getTotalReviews);
+router.get("/:userId", verifyToken, getUserReviews);
 
 export default router;
--- server/src/users/user.controller.js
+++ server/src/users/user.controller.js
@@ -44,7 +44,8 @@ export const loginUser = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: false,
-      sameSite: "None",
+      sameSite: "lax",
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
     // res.status(200).send({
     //   message: "Login successful",
